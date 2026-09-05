23e1cc752339b57de82b718d0f735e2722ebb0fb
Sitt-Paing <sittp9288@gmail.com>
2026-09-05 14:41:36 +0630

    Update navbar.ts

--- Bookshop.client/src/app/shared/components/navbar/navbar.ts
+++ Bookshop.client/src/app/shared/components/navbar/navbar.ts
@@ -3,23 +3,29 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { CategoryModel } from '../../../core/models/category.model';
-import { AuthService } from '../../../core/services/auth.service';
-import { CartService } from '../../../core/services/cart.service';
 import { CategoryService } from '../../../core/services/category.service';
+import { SharedService } from '../../../core/services/shared.service';
+import { CartState } from '../../../core/state/cart.state';
 import { MmkCurrencyPipe } from '../../pipes/mmk-currency.pipe';
 import { Logo } from '../logo/logo';
+
+import { TranslationService } from '../../../core/services/translation.service';
+import { TranslatePipe } from '../../../core/pipes/translate.pipe';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule, Logo, MmkCurrencyPipe],
+  imports: [CommonModule, RouterLink, RouterLinkActive, FormsModule, Logo, MmkCurrencyPipe, TranslatePipe],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  host: {
+    class: 'block',
+  },
 })
 export class Navbar implements OnInit {
-  readonly authService = inject(AuthService);
-  readonly cartService = inject(CartService);
+  readonly sharedService = inject(SharedService);
+  readonly cartService = inject(CartState);
   readonly categoryService = inject(CategoryService);
+  readonly translationService = inject(TranslationService);
   private readonly router = inject(Router);
 
   categories: CategoryModel[] = [];
