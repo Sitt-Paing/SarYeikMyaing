7e4ed675fa661bbc00f05fc14dabd03ee1d84021
Sitt-Paing <sittp9288@gmail.com>
2026-09-05 14:41:21 +0630

    Update footer.ts

--- Bookshop.client/src/app/shared/components/footer/footer.ts
+++ Bookshop.client/src/app/shared/components/footer/footer.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Logo } from '../logo/logo';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, Logo],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {}
+
