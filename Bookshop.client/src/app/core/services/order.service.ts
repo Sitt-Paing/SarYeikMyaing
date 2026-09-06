33fbff2f4f76086541f3f0f6d73b99066dd5d197
Sitt-Paing <sittp9288@gmail.com>
2026-09-07 00:00:04 +0630

    Update order.service.ts

--- Bookshop.client/src/app/core/services/order.service.ts
+++ Bookshop.client/src/app/core/services/order.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { CreateOrderModel, OrderModel } from '../models/order.model';
+import { CreateOrderModel } from '../models/order.model';
 import { RootModel } from '../models/root.model';
 
 @Injectable({
@@ -16,18 +16,45 @@ export class OrderService {
     return this.http.get<RootModel>(url);
   }
 
+  getPending(): Observable<RootModel> {
+    const url = `${environment.main_url}/Order/pending`;
+    return this.http.get<RootModel>(url);
+  }
+
   getById(id: number | string): Observable<RootModel> {
     const url = `${environment.main_url}/Order/${id}`;
     return this.http.get<RootModel>(url);
   }
 
-  create(model: CreateOrderModel): Observable<RootModel> {
+  uploadSlip(file: File): Observable<RootModel> {
+    const formData = new FormData();
+    formData.append('file', file);
+    const url = `${environment.main_url}/Order/upload-slip`;
+    return this.http.post<RootModel>(url, formData);
+  }
+
+  create(model: any): Observable<RootModel> {
     const url = `${environment.main_url}/Order`;
     return this.http.post<RootModel>(url, model);
   }
 
-  updateStatus(id: number | string, status: string): Observable<RootModel> {
-    const url = `${environment.main_url}/Order/${id}/status`;
-    return this.http.put<RootModel>(url, { status });
+  approve(id: number | string): Observable<RootModel> {
+    const url = `${environment.main_url}/Order/${id}/approve`;
+    return this.http.put<RootModel>(url, {});
+  }
+
+  reject(id: number | string): Observable<RootModel> {
+    const url = `${environment.main_url}/Order/${id}/reject`;
+    return this.http.put<RootModel>(url, {});
+  }
+
+  update(id: number | string, model: any): Observable<RootModel> {
+    const url = `${environment.main_url}/Order/${id}`;
+    return this.http.put<RootModel>(url, model);
+  }
+
+  delete(id: number | string): Observable<RootModel> {
+    const url = `${environment.main_url}/Order/${id}`;
+    return this.http.delete<RootModel>(url);
   }
 }
