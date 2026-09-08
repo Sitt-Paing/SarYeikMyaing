514815627d5bbed7147e125c8c7a3f1a1378ca1d
Sitt-Paing <sittp9288@gmail.com>
2026-09-08 21:00:33 +0630

    Update cart.ts

--- Bookshop.client/src/app/pages/cart/cart.ts
+++ Bookshop.client/src/app/pages/cart/cart.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CartState } from '../../core/state/cart.state';
 import { MmkCurrencyPipe } from '../../shared/pipes/mmk-currency.pipe';
+import { TranslatePipe } from '../../core/pipes/translate.pipe';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule, MmkCurrencyPipe],
+  imports: [CommonModule, RouterLink, FormsModule, MmkCurrencyPipe, TranslatePipe],
   templateUrl: './cart.html',
-  styleUrl: './cart.scss',
 })
 export class Cart {
   readonly cartService = inject(CartState);
