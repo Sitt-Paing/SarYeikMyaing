dfeec5384d687490c10742990bdd7862fda4f863
Sitt-Paing <sittp9288@gmail.com>
2026-09-08 21:17:49 +0630

    Update logo.ts

--- Bookshop.client/src/app/shared/components/logo/logo.ts
+++ Bookshop.client/src/app/shared/components/logo/logo.ts
@@ -7,7 +7,6 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './logo.html',
-  styleUrl: './logo.scss',
 })
 export class Logo {
   size = input<'sm' | 'md' | 'lg'>('md');
