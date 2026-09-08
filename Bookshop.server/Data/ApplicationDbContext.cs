ddd15275a56716df352e36b5ba86b5ee7506695a
Sitt-Paing <sittp9288@gmail.com>
2026-09-08 21:22:42 +0630

    Update ApplicationDbContext.cs

--- Bookshop.server/Data/ApplicationDbContext.cs
+++ Bookshop.server/Data/ApplicationDbContext.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookshop.Data;
 
-public class ApplicationDbContext: DbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Custom schema/table name mappings can be configured here if desired.
+        // By default, ASP.NET Core Identity configures AspNetUserRoles, AspNetUsers, AspNetRoles, etc.
     }
 }
