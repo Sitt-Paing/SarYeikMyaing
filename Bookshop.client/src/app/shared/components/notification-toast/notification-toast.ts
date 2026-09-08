1ae15ed94f7bdcb4dcd9ee01778901e09478fd88
Sitt-Paing <sittp9288@gmail.com>
2026-09-08 21:21:26 +0630

    Update notification-toast.ts

--- Bookshop.client/src/app/shared/components/notification-toast/notification-toast.ts
+++ Bookshop.client/src/app/shared/components/notification-toast/notification-toast.ts
@@ -7,6 +7,5 @@ import { ToastModule } from 'primeng/toast';
   standalone: true,
   imports: [CommonModule, ToastModule],
   templateUrl: './notification-toast.html',
-  styleUrl: './notification-toast.scss',
 })
 export class NotificationToast {}
