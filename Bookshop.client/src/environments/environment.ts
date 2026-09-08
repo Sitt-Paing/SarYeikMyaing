012279fc6b1b8d068a57abee5938cbb582d8c5a4
Sitt-Paing <sittp9288@gmail.com>
2026-09-08 21:21:35 +0630

    Update environment.ts

--- Bookshop.client/src/environments/environment.ts
+++ Bookshop.client/src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
   debug: true,
   default_pagination: [10, 20, 30, 50, 100],
   default_toastKey: 'globalMessage',
-  appName: 'စာအုပ်စင် - SarOatSin Bookshop',
+  appName: 'Sar Yeik Myaing - စာရိပ်မြိုင် Bookshop',
 };
