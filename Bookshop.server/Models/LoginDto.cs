f00be611fdfdadbf494d33b4e7aeadc87f11e4f4
Sitt-Paing <sittp9288@gmail.com>
2026-09-08 21:24:58 +0630

    Update LoginDto.cs

--- Bookshop.server/Models/LoginDto.cs
+++ Bookshop.server/Models/LoginDto.cs
@@ -3,6 +3,6 @@ namespace Bookshop.Models;
 public class LoginDto
 {
     public string UserNameOrEmailOrPhone { get; set; } = null!;
-
     public string Password { get; set; } = null!;
+    public bool RememberMe { get; set; } = false;
 }
