e1b0d1fba374a58a1aaaf0c279974e2621d80cbe
Sitt-Paing <sittp9288@gmail.com>
2026-09-12 14:47:58 +0630

    Update environment.ts

--- Bookshop.client/src/environments/environment.ts
+++ Bookshop.client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   docker: false,
-  main_url: 'https://localhost:7270/api',
+  main_url: '/core/api',
   debug: true,
   default_pagination: [10, 20, 30, 50, 100],
   default_toastKey: 'globalMessage',
